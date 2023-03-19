3fcea47948c3e5d90f0efec1c9dbc2d951be6c9a
Mai Lan <phammailan@gmail.com>
2023-03-19 19:09:53 +0700

    Init

---
+++ src/main/java/com/mycompany/lab01/Lab01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ngoth
+ */
+public class Lab01 {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        char c = '\uD83d';
+//        System.out.println("c = " + c);
+
+    }
+    
+}
